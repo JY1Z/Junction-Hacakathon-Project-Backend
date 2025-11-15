d7e9f86be533e036fbaa107d6646fd1c79712ec8
jiayuez <Jiayue.Zheng@metropolia.fi>
2025-11-15 14:26:46 +0200

    feat(api): update gemini.py with comments

---
+++ app/services/gemini.py
@@ -0,0 +1,11 @@
+# app.services.gemini.py
+
+# Receive the order from the frontend.
+
+# Get the customer id and all unavailable items.
+
+
+# Database: products from the same categories as the unavailable items +  customer behavior & description
+
+# Gemini API: generate product recommendations for replacements
+
